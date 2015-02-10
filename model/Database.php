2bd14b4158c59b7fc4b07b3f8bd7953599895f8f
Denis Makhlin <denismakhlin@gmail.com>
2015-02-10 14:59:32 -0800

    2nd Commit

--- model/Database.php
+++ model/Database.php
@@ -1,5 +1,7 @@
 <?php
+
 class Database {
+
 //Below are global variables that are set as private so they can only be
 //accessed within this class
     private $connection;
@@ -7,24 +9,40 @@ class Database {
     private $username;
     private $password;
     private $database;
+
 //Below I am adding information into my global variables to add to my object
     public function __construct($host, $username, $password, $database) {
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
-           
     }
-    
+
     public function openConnection() {
-        
+//    Below Is the code that I established my connection in
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->connection->connect_error) {
+//Below is code that kills my program if I have an error and it says what the
+//error was
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
     }
-    
+//Below is the code rhat closes my connection
     public function closeConnection() {
-        
+        if(isset($this->connection)) {
+            $this->coonnection->close();
+        }
     }
-    
+
     public function query($string) {
+        $this->openConnection();
         
+        $query = $this->connection->query($string);
+        
+        $this->closeConnection();
+        
+        return $query;
     }
+
 }
