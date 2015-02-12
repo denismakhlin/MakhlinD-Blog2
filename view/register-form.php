22777791993199f8a981b7a52031c158924573cb
Denis Makhlin <denismakhlin@gmail.com>
2015-02-12 14:56:27 -0800

    5th Commit

---
+++ view/register-form.php
@@ -0,0 +1,23 @@
+<!--This is the code that is the visual part of my registration form-->
+<form>
+    <!--Below is my email text area-->
+    <div>
+        <label for="email">Email: </label>    
+        <input type="text" name="email"/>
+    </div>
+    <!--Below is my username text area-->
+    <div>
+        <label for="username">Username: </label>
+        <input type="text" name="username"/>
+    </div>
+    <!--Below is my password text area-->
+    <div>
+        <label for="password">Password: </label>
+        <input type="password" name="password"/>
+    </div>
+    <!--Below is my submit button which send the above code to my database-->
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form> 
+    
